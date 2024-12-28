6a2080494d339717ea7c881f8ca973ebb6076421
Yeeswanth <yeesw@YeeswanthLatha.com>
2024-12-28 17:30:00 +0530

    Create automated tst for add to cart

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
+loadFromStorage();
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart'));
 if(!cart){
     cart = [{
       productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -11,12 +15,13 @@ if(!cart){
       deliveryOptionId: '2'
     }];
 }
-
+}
 function saveToStorage () {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function addToCart(productId){
+  console.log('addToCart called');
   let matchingItem;
 
     cart.forEach((item) => {
