a753754e7ab8ed01357b9846ffd4ba23768dddc9
Yeeswanth <yeesw@YeeswanthLatha.com>
2025-01-09 20:51:39 +0530

    Use Inheritance and Polymorphism in clothing

--- data/products.js
+++ data/products.js
@@ -29,6 +29,27 @@ class Product {
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`
   }
+
+  extraInfoHTML(){
+    return '';
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+      Size Chart
+      </a>
+    `;
+  }
 }
 
 export const products = [
@@ -717,6 +738,9 @@ export const products = [
           "rain"
         ]}
 ].map((productDetails) =>{
+  if(productDetails.type === "clothing"){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
