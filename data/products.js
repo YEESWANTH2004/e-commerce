ad55422e6b731f369a1362781ee8a4475f95c7e2
Yeeswanth <yeesw@YeeswanthLatha.com>
2025-01-11 22:35:23 +0530

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -52,11 +52,35 @@ class Clothing extends Product {
   }
 }
 
+export let products = [];
+
+export function loadProducts (fun) {
+ const xhr =  new XMLHttpRequest();
+
+xhr.addEventListener('load', () =>{
+  products = JSON.parse(xhr.response).map((productDetails) =>{
+    if(productDetails.type === "clothing"){
+      return new Clothing(productDetails);
+    }
+    return new Product(productDetails);
+  });
+
+  console.log('load Products');
+
+  fun();
+});
+
+ xhr.open('GET', 'https://supersimplebackend.dev/products');
+ xhr.send(); 
+}
+
+
+
 // const date = new Date();
 
 // console.log(date)
 // console.log(date.toLocaleTimeString());
-
+/*
 export const products = [
  {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -748,4 +772,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
-
+*/
